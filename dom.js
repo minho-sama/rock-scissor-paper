2e856363fe9b61d83893f7a06d5510d49065aea9
minho-sama <n.a.minh1106@gmail.com>
2021-03-22 21:51:56 +0100

    Copied everything from windows, final version

---
+++ dom.js
@@ -0,0 +1,121 @@
+let playerScore = 0
+let Pcscore = 0
+
+rockImg.onclick = chooseRock
+paperImg.onclick= choosePaper
+scissorImg.onclick = chooseScissor
+
+const playerOrpc = document.querySelector('#playerOrpc')
+const score = document.querySelector('#score')
+const winnerIMG = document.querySelector('#winnerIMG')
+const resultBox = document.querySelector('.result h3')
+
+function chooseRock(){
+    playerSelect = 'rock'
+    console.log('Player: ' + playerSelect)
+
+    playRound()
+}
+
+function choosePaper(){
+    playerSelect = 'paper'
+    console.log('Player: ' + playerSelect)
+
+    playRound()
+}
+
+function chooseScissor(){
+    playerSelect = 'scissor'
+    console.log('Player: ' + playerSelect)
+
+    playRound()
+}
+
+function playRound(){
+    computerPlay()
+    console.log('PC: ' + computerSelect)
+
+    if(playerScore + Pcscore == 5){
+        score.textContent = `score: ${playerScore} - ${Pcscore}`
+            if(playerScore > Pcscore){
+                playerWinChange()
+
+            }
+            else{
+                pcWinChange()
+            }
+            playerScore = 0
+            Pcscore = 0
+    
+    }
+
+    else if (playerSelect == computerSelect){
+        
+        console.log("it's a draw!")
+        playerOrpc.textContent = 'winner of this round: No one'
+        winnerIMG.setAttribute('src', 'znemo.jpg')
+        resultBox.setAttribute("style", "background-color: #add8e6;");
+
+    }
+
+    else if (playerSelect == "paper" && computerSelect != "scissor" ||
+             playerSelect == "rock" && computerSelect != "paper" ||
+             playerSelect == "scissor" && computerSelect != "rock"){
+            
+            playerWinChange()
+             
+    }
+            
+    else{
+        pcWinChange()
+    }
+    score.textContent = `score: ${playerScore} - ${Pcscore}`
+
+    console.log('player score: ' + playerScore)
+    console.log('pc score: ' + Pcscore)
+
+}
+
+function computerPlay(){
+    ai = Math.floor(Math.random()*10+3);
+    if (ai <= 4){
+        computerSelect = 'rock'
+    }
+    
+    else if(ai > 4 && ai <= 8 ){
+        computerSelect = 'paper'
+    }
+    else{
+        computerSelect = 'scissor'
+    }
+}
+
+function playerWinChange(){
+
+   if (playerScore+Pcscore == 5){
+        winnerIMG.setAttribute('src', 'zwinmagneto.jpg')
+        playerOrpc.textContent = 'Congrats! You won the game'
+   }
+   else{
+        playerScore++
+        winnerIMG.setAttribute('src', 'zdavid.png')
+        playerOrpc.textContent = 'winner of this round: You'
+        console.log(`player wins! ${playerSelect} beats ${computerSelect}!`)
+   }
+   resultBox.setAttribute("style", "background-color: #a2f3a2;");
+}
+
+function pcWinChange(){
+
+    if (playerScore+Pcscore == 5){
+        winnerIMG.setAttribute('src', 'zvisionwin.jpg')
+        playerOrpc.textContent = 'Congrats! A fkin computer beat you'
+    }
+    else{
+        Pcscore++
+        winnerIMG.setAttribute('src', 'zvision.jpg')
+        playerOrpc.textContent = 'winner of this round: PC'
+        console.log(`computer wins! ${computerSelect} beats ${playerSelect}!`)
+    }
+    resultBox.setAttribute("style", "background-color: rgb(236, 72, 72);");
+}
